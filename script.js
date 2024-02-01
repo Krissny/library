4d5a4e610f87524ba35d355da0684f2f9dd2bf37
Krissny <110375406+Krissny@users.noreply.github.com>
2024-02-01 18:14:06 +0530

    Add script file

---
+++ script.js
@@ -0,0 +1,11 @@
+const library = [
+    {"Harry potter": "JK Rowling",
+        read: true,
+    },
+    { "My Experiments With truth": "Mahatma Gandhi",
+      read: false
+    }
+];
+function user_input(){
+  
+}
